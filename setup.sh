e51d96aeb3f52c2cd30ce3977d0415958a5aaa85
Aank is ME <email@aank.me>
2024-11-22 00:20:44 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-apt update && apt upgrade -y
-apt install -y git python3-pip
+sudo apt update && apt upgrade -y
+sudo apt install python3 -y
+sudo apt install -y git python3-pip
 pip3 install -r requirements.txt
